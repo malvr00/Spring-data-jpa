383383a232cf98d5ede6a4296ba04cefa0af37e1
조용일 <yedn1128@naver.com>
2022-04-21 23:09:41 +0900

    공통 인터페이스 설정

--- src/main/java/study/datajpa/repository/TeamRepository.java
+++ src/main/java/study/datajpa/repository/TeamRepository.java
@@ -1,46 +1,8 @@
 package study.datajpa.repository;
 
-import org.springframework.stereotype.Repository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import study.datajpa.entity.Team;
 
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
-import java.util.List;
-import java.util.Optional;
-
-@Repository
-public class TeamRepository {
-
-    @PersistenceContext
-    private EntityManager em;
-
-    // 생성
-    public Team save(Team team){
-        em.persist(team);
-        return team;
-    }
-
-    // 삭제
-    public void delete(Team team){
-        em.remove(team);
-    }
-
-    // 전체 찾기
-    public List<Team> findAll(){
-        return em.createQuery("select t from Team t", Team.class)
-                .getResultList();
-    }
-
-    // 단일 검색
-    public Optional<Team> findById(Long id){
-        Team team = em.find(Team.class, id);
-        return Optional.ofNullable(team);
-    }
-
-    // 팀 수
-    public long count(){
-        return em.createQuery("select count(t) from Team t", Long.class)
-                .getSingleResult();
-    }
-
+// @Repository 생략가능
+public interface TeamRepository extends JpaRepository<Team, Long> {
 }
